df01c498823af7b88d7d67645152934337c423d4
mguetlein <martin.guetlein@gmail.com>
2011-05-24 19:41:32 +0200

    simplified line/roc plot

--- lib/plot_lines.rb
+++ lib/plot_lines.rb
@@ -7,40 +7,18 @@
 # Usage: See below.
 
 module RubyPlot
+  
+  class LinePlotData
+    attr_accessor :name, :x_values, :y_values, :faint, :labels
+    def initialize(params)
+      params.each{|k,v| send((k.to_s+"=").to_sym,v)}
+    end
+  end
 
-  def self.plot_lines(path, title, x_lable, y_lable, names, x_values, y_values, faint=nil, labels=nil)
+  def self.plot_lines(path, title, x_lable, y_lable, plot_data )
     
-    LOGGER.debug "plot lines -- "+names.inspect
-    LOGGER.debug "plot lines -- "+x_values.inspect
-    LOGGER.debug "plot lines -- "+y_values.inspect
-    LOGGER.debug "plot lines -- "+labels.inspect
-    
-    data = []
-    (0..x_values.size-1).each do |i|
-      data << y_values[i]
-      data << x_values[i]
-    end
-    
-    #Main
-    STDOUT.sync = true
-    # -----------------------------------------------------
-    # checking input
-    # -----------------------------------------------------
-    # check parameters
-    status=false
-    LOGGER.debug "#{names.length} algs entered"
-    
-    #LOGGER.debug names.inspect
-    #LOGGER.debug data.inspect
-    
-    if names.length != data.length/2
-        status=true
-    end
-    
-    if status
-      raise "Usage: svg_roc_plot (path(?), title(string), x-lable(string), y-lable(sting), algorithms(array), true_pos_data1(array), false_pos_data1(array), ...,  true_pos_data_n(array), false_pos_data_n(array))\n"+
-            "       Only pairs of data are allowed but at least one.\n"+
-            "       Each data array has to provide one float/int number from 0 to 100 per entry."
+    plot_data.each do |d|
+      LOGGER.debug "plot lines -- "+d.name+" - "+d.x_values.inspect+" - "+d.y_values.inspect+" - "+d.labels.inspect
     end
     
     # gnuplot check
@@ -52,18 +30,18 @@ module RubyPlot
             "sudo apt-get install gnuplot"
     end
     
-    dat_number=0
-    
     output_dat_arr = Array.new
-    
     
     # -----------------------------------------------------
     # create *.dat files of imported data for gnuplot
     # -----------------------------------------------------
     # write true/false arrays to one array
-    for i in 0..names.length-1#/2-1
-      true_pos_arr = data[i*2]
-      false_pos_arr = data[i*2+1]
+    #for i in 0..plot_data.length-1#/2-1
+    plot_data.size.times do |i|
+      
+      true_pos_arr = plot_data[i].y_values
+      false_pos_arr = plot_data[i].x_values
+      
       #check length of input files
       if true_pos_arr.length == false_pos_arr.length
         #LOGGER.debug "Same length!"
@@ -73,11 +51,11 @@ module RubyPlot
             if  numeric?(false_pos_arr[j].to_s.tr(',', '.')) && false_pos_arr[j].to_s.tr(',', '.').to_f <= 100 && false_pos_arr[j].to_s.tr(',', '.').to_f >= 0
               output_dat_arr[j] = "#{true_pos_arr[j]} #{false_pos_arr[j]}"
             else
-              raise "The data of #{names[i]}  has not the right formatin at position #{j}\n"+
+              raise "The data #{plot_data[i].inspect}  has not the right formatin at position #{j}\n"+
                     "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
             end
           else
-            raise "The data of #{names[i]}  has not the right formatin at position #{j}+\n"
+            raise "The data #{plot_data[i].inspect}  has not the right formatin at position #{j}+\n"
                    "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
           end
         end
@@ -92,7 +70,7 @@ module RubyPlot
         output_dat_arr.clear
             
       else
-        raise "Data pair of #{names[i]} have no the same number of elements."
+        raise "num x-values != y-values: "+plot_data[i].inspect
       end
     end
     
@@ -133,33 +111,33 @@ module RubyPlot
     output_plt_arr.push ""
     output_plt_arr.push "# Draws the plot and specifies its appearance ..."
     
-    if labels!=nil
-      type = 1
-      labels.each do |label|
-        if label!=nil
+    type = 1
+    plot_data.each do |p|
+      if p.labels
+        p.labels.each do |label|
           l = label[0]
           x = label[1]
           y = label[2]
-          puts l.to_s+" "+x.to_s+" "+y.to_s
+          #puts l.to_s+" "+x.to_s+" "+y.to_s
           #output_plt_arr.push "set label \"("+x.to_s+","+y.to_s+") "+l.to_s+"\" at first 25, first 40"
           output_plt_arr.push "set label \""+l.to_s+"\" at first "+x.to_s+", first "+y.to_s+" front offset 1,-1 tc lt "+type.to_s
           output_plt_arr.push "set arrow from "+(x+3).to_s+","+(y-3).to_s+" to "+(x+1).to_s+","+(y-1).to_s+" lt "+type.to_s
         end
-        type += 1
-      end
+      end    
+      type += 1
     end
     
     output_plt_arr.push "plot \\"#'random_0.dat' using 1:2 title 'random' with lines lw 1, \\"
     i = 0
-    for i in 0..names.length-1
+    for i in 0..plot_data.length-1
       
       #style = grey[i] ? "lw 1.5 lt 0" : "lw 3" 
-      style = faint!=nil && faint[i] ? "lw 2" : "lw 4"
+      style = plot_data[i].faint ? "lw 2" : "lw 4"
       
-      if i == names.length-1
-        output_plt_arr.push " 'data#{i}.dat'  using 2:1 title '#{names[i]}' with lines #{style}"
+      if i == plot_data.length-1
+        output_plt_arr.push " 'data#{i}.dat'  using 2:1 title '#{plot_data[i].name}' with lines #{style}"
       else
-        output_plt_arr.push " 'data#{i}.dat'  using 2:1 title '#{names[i]}' with lines #{style}, \\"
+        output_plt_arr.push " 'data#{i}.dat'  using 2:1 title '#{plot_data[i].name}' with lines #{style}, \\"
       end
     end
     output_plt_arr.push ""
@@ -191,7 +169,7 @@ module RubyPlot
     # -----------------------------------------------------
     `rm config.plt`
     LOGGER.debug "config.plt removed."
-    for i in 0..names.length-1
+    for i in 0..plot_data.length-1
       `rm data#{i}.dat`
       LOGGER.debug "data#{i}.dat removed."
     end
@@ -199,8 +177,13 @@ module RubyPlot
  
   def self.test_plot_lines
     #plot_lines("/tmp/result.svg" , "name of title", "x-values", "y-values", ["name", "test", "bla"], [[20,60,80], [10,25,70,95], [12,78,99]], [[15,50,90],[20,40,50,70],[34,89,89]],[true,false,true])
+    #plot_lines("/tmp/result.png" , "name of title", "x-values", "y-values", ["name", "test", "bla"], [[20,60,80], [10,25,70,95], [12,78,99]], [[15,50,90],[20,40,50,70],[34,89,89]],[true,false,true],[nil,["confidence",25,40]])
     
-    plot_lines("/tmp/result.png" , "name of title", "x-values", "y-values", ["name", "test", "bla"], [[20,60,80], [10,25,70,95], [12,78,99]], [[15,50,90],[20,40,50,70],[34,89,89]],[true,false,true],[nil,["confidence",25,40]])
+    plot_data = []
+    plot_data << LinePlotData.new({ :name => "name", :x_values => [20,60,80], :y_values => [15,50,90] })
+    plot_data << LinePlotData.new({ :name => "test", :x_values => [10,25,70,95], :y_values => [20,40,50,70], :labels => [["confidence",25,40]] })
+    plot_data << LinePlotData.new({ :name => "bla", :x_values => [12,78,99], :y_values =>[34,89,89], :faint => true })
+    plot_lines("/tmp/result.png" , "name of title", "x-values", "y-values", plot_data)
   end
  
   private
